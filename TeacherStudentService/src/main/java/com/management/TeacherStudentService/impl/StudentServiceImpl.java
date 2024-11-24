8bcfe846e4f2832097ded4cfbf69095e1eaba3ac
BenovseBalammedova <benovsebektas@gmail.com>
2024-11-25 02:30:04 +0400

    commit

--- TeacherStudentService/src/main/java/com/management/TeacherStudentService/impl/StudentServiceImpl.java
+++ TeacherStudentService/src/main/java/com/management/TeacherStudentService/impl/StudentServiceImpl.java
@@ -2,15 +2,20 @@ package com.management.TeacherStudentService.impl;
 
 import com.management.TeacherStudentService.entity.Student;
 import com.management.TeacherStudentService.repository.StudentRepository;
-import com.management.TeacherStudentService.service.CommonService;
 import com.management.TeacherStudentService.service.StudentService;
+import com.management.TeacherStudentService.dto.StudentRequestDto;
+import com.management.TeacherStudentService.dto.StudentResponceDto;
+import com.management.TeacherStudentService.mapper.StudentMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class StudentServiceImpl implements StudentService, CommonService<Student> {
+
+public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository repository;
 
@@ -18,67 +23,65 @@ public class StudentServiceImpl implements StudentService, CommonService<Student
         this.repository = repository;
     }
 
+
     @Override
-    public List<Student> getAll() {
-        return repository.findAll();
+    public List<StudentResponceDto> getAll() {
+        List<Student> students = repository.findAll();
+        return StudentMapper.entityToList(students);
     }
 
     @Override
-    public Student getById(Long id) {
-        return repository.findById(id).orElseThrow();
+    public StudentResponceDto getById(Long id) {
+        StudentResponceDto student = StudentMapper
+                .entityToDto(repository.findById(id).orElseThrow());
+
+        return student;
     }
 
     @Override
     public void delete(Long id) {
+
         repository.deleteById(id);
     }
 
     @Override
-    public void save(Student obj) {
-        repository.save(obj);
+    public void save(StudentRequestDto obj) {
+        Student student = StudentMapper.dtoToEntity(obj);
+        repository.save(student);
     }
 
     @Override
-    public void update(Long id, Student obj) {
-        Student findedStudent=repository.findById(id).orElseThrow();
-        findedStudent.setFullName(obj.getFullName());
-        findedStudent.setAge(obj.getAge());
-        findedStudent.setGpa(obj.getGpa());
-        findedStudent.setMajor(obj.getMajor());
-        findedStudent.setEmail(obj.getMajor());
-        findedStudent.setCourseYear(obj.getCourseYear());
+    public void update(Long id, StudentRequestDto obj) {
+
+        Student student = StudentMapper.dtoToEntity(obj);
+        Student student1 = repository.findById(id).orElseThrow();
+        student1.setFullName(student.getFullName());
+        student1.setEmail(student.getEmail());
+        student1.setAge(student.getAge());
+        student1.setGpa(student.getGpa());
+        student1.setCourseYear(student.getCourseYear());
+        student1.setPassword(student.getPassword());
+        student1.setMajor(student.getMajor());
+        repository.save(student1);
     }
 
     @Override
-    public List<Student> getByMajor(String major) {
-        List<Student> students=new ArrayList<>();
-        for(Student s: repository.findAll()){
-            if(s.getMajor().equals(major)){
-                students.add(s);
-            }
-        }
-        return students;
+    public List<StudentResponceDto> getByMajor(String major) {
+        List<Student> students = repository.getByMajor(major);
+        return StudentMapper.entityToList(students);
+
     }
 
     @Override
-    public List<Student> getByCourseYear(Short courseYear) {
-        List<Student> students=new ArrayList<>();
-        for(Student s: repository.findAll()){
-            if(s.getCourseYear().equals(courseYear)){
-                students.add(s);
-            }
-        }
-        return students;
+    public List<StudentResponceDto> getByCourseYear(Short courseYear) {
+        List<Student> students = repository.getByCourseYear(courseYear);
+        return StudentMapper.entityToList(students);
+
     }
 
     @Override
-    public List<Student> getByGpaInterval(Double gpa1, Double gpa2) {
-        List<Student> students=new ArrayList<>();
-        for(Student s: repository.findAll()){
-            if(s.getGpa()>gpa1&&s.getGpa()<gpa2){
-                students.add(s);
-            }
-        }
-        return students;
+    public List<StudentResponceDto> getByGpaInterval(Double gpa1, Double gpa2) {
+        List<Student> students = repository.getByGpaInterval(gpa1, gpa2);
+        return StudentMapper.entityToList(students);
     }
 }
--- TeacherStudentService/src/main/java/com/management/TeacherStudentService/repository/StudentRepository.java
+++ TeacherStudentService/src/main/java/com/management/TeacherStudentService/repository/StudentRepository.java
@@ -1,10 +1,18 @@
 package com.management.TeacherStudentService.repository;
 
 
+import com.management.TeacherStudentService.dto.StudentResponceDto;
 import com.management.TeacherStudentService.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    public List<Student> getByMajor(String major);
+    public List<Student> getByCourseYear(Short courseYear);
+    public List<Student> getByGpaInterval(Double gpa1, Double gpa2);
+
 }
